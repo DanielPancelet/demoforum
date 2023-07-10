a0d369650c9f03e218977947804a05dbc7624ec4
DanielPancelet <85060864+DanielPancelet@users.noreply.github.com>
2023-07-10 09:34:26 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ export default {
     testLodash(){
       const array = [1];
       const other = _.concat(array, 2, [3], [[4]]);
-      console.log(other)
+      // console.log(other)
     }
   },
   mounted() {
